47cf9c430636a824d914635cf6820c4e381a267c
Sadi Pritom <sadipritom@gmail.com>
2022-11-11 15:28:01 +0600

    N-Tier Architecture done

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,160 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+//using BulkyBookWeb.Data;
+//using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //private readonly ApplicationDbContext _db;
+        private readonly ICategoryRepository _db;
+        public CategoryController(ICategoryRepository db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.GetAll();
+            return View(objCategoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("CustomError", "The display order cannot Exatly match the name");
+            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The display order cannot Exatly match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+           /* var CategoryFromDb = _db.Categories.Find(id)*/;
+            var categoryFromDbFirst=_db.GetFirstOrDefault(u=>u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (categoryFromDbFirst == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(categoryFromDbFirst);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("CustomError", "The display order cannot Exatly match the name");
+            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The display order cannot Exatly match the name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+           // var CategoryFromDb = _db.Categories.Find(id);
+            var CategoryFromDbFirst=_db.GetFirstOrDefault(u=>u.Id == id);
+            //var ategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (CategoryFromDbFirst == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(CategoryFromDbFirst);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("CustomError", "The display order cannot Exatly match the name");
+            //}
+
+
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "The display order cannot Exatly match the name");
+            //}
+
+            //if (ModelState.IsValid)
+            //{
+            //    _db.Categories.Update(obj);
+            //    _db.SaveChanges();
+            //    return RedirectToAction("Index");
+
+            //}
+            //return View(obj);
+
+
+            var obj = _db.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            _db.Remove(obj);
+            _db.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
