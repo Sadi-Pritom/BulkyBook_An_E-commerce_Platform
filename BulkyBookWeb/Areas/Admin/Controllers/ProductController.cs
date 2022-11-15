04c25eaac0a1cd1cbd97825f36323cea2a400d1b
Sadi Pritom <sadipritom@gmail.com>
2022-11-16 02:53:23 +0600

    Product Upset controller created

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,148 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+//using BulkyBookWeb.Data;
+//using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        //private readonly ApplicationDbContext _db;
+        //private readonly ICategoryRepository _db;
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+      
+
+
+        //Get
+        public IActionResult Upsert(int? id)
+        {
+
+            Product product = new();
+
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value=u.Id.ToString()
+                }
+                );
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+                );
+            if (id == null || id == 0)
+            {
+               //create product
+               return View(product);
+            }
+            else
+            {
+                //update product
+            }
+          
+
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("CustomError", "The display order cannot Exatly match the name");
+            //}
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "The display order cannot Exatly match the name");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+
+        }
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // var CategoryFromDb = _db.Categories.Find(id);
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            //var ategoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("CustomError", "The display order cannot Exatly match the name");
+            //}
+
+
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "The display order cannot Exatly match the name");
+            //}
+
+            //if (ModelState.IsValid)
+            //{
+            //    _db.Categories.Update(obj);
+            //    _db.SaveChanges();
+            //    return RedirectToAction("Index");
+
+            //}
+            //return View(obj);
+
+
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
